8dabe3d9d25992769d95e83ccc213efcc6235f6e
JOÃO GUILHERME DE LIMA OLIVEIRA <20241148060011@ifrn.local>
2025-04-22 08:28:49 -0300

    somaVetor recurssividade

--- ex-recursividade.ts
+++ ex-recursividade.ts
@@ -8,12 +8,22 @@
 
 // console.log(soma(5))
 
-function fatorial(n : number) : number{
-    if(n == 1 || n == 0){
-        return 1; // caso base
+// function fatorial(n : number) : number{
+//     if(n == 1 || n == 0){
+//         return 1; // caso base
+//     }else{
+//         return n * fatorial(n-1); // recursividade 
+//     }
+// }
+
+// console.log(fatorial(1))
+
+function somaVetor(...lista: number[]) : number{
+    if(lista.length === 0){
+        return 0
     }else{
-        return n * fatorial(n-1); // recursividade 
+        return lista[0] + somaVetor(...lista.slice(1)) // slice cria uma nova lista sem o primeiro elemento, fazendo isso todas as vezes
     }
 }
 
-console.log(fatorial(3))
+console.log(somaVetor(1,2,3,4,5));
